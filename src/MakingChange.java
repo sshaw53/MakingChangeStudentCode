81f6dba4ff08715af6bc7f8cc72f4110c56ca8e4
sierrashaw <sierra.shaw@menloschool.org>
2025-01-27 21:40:30 -0800

    first try part 1

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 /**
  * The class Making Change solves a classic problem:
  * given a set of coins, how many ways can you make change for a target amount?
  *
  * @author Zach Blick
- * @author [YOUR NAME HERE]
+ * @author SIERRA SHAW
  */
 
 public class MakingChange {
@@ -12,6 +14,32 @@ public class MakingChange {
      *  for any given total with any given set of coins.
      */
     public static long countWays(int target, int[] coins) {
-        return 0;
+        Arrays.sort(coins);
+        long[] currentCounts = new long[target + 1];
+        long count = 0;
+        int coinLen = coins.length;
+
+        for (int i = 0; i < coinLen; i++){
+            count += findCount(coins, currentCounts, target, i, coins[i], coinLen);
+        }
+
+        return count;
+    }
+
+    public static long findCount(int[] coins, long[] currentCounts, int target, int currentIdx, int total, int coinLen) {
+        if (total > target) {
+            return 0;
+        }
+        else if (total == target) {
+            return currentCounts[target];
+        }
+
+        long count = 0;
+
+        for (int i = currentIdx; i < coinLen; i++){
+            // find some way to take advantage of the dynamic programming
+            count += findCount(coins, currentCounts, target, i, (total + coins[i]), coinLen);
+            currentCounts[total + coins[i]] += count;
+        }
     }
 }
